2eae6655fe6e740e923f412f34e4cbe57b3a476e
Jorge Arevalo <jorgeas80@gmail.com>
2016-09-27 02:45:31 +0200

    Añadido ejercicio sobre ts

---
+++ exercises/01_typescript/greeter.js
@@ -0,0 +1,15 @@
+var Student = (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jorge", "Luis.", "Arévalo");
+document.body.innerHTML = greeter(user);
+//# sourceMappingURL=greeter.js.map
